9b0c2552f596bc7cf8aaec7bb0a17dffa9b66f3e
saketkum11 <kumarsaket601@gmail.com>
2022-04-22 00:34:41 +0530

    added:home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,127 @@
 import { Header } from "../index";
+
 const Home = () => {
   return (
     <>
       <Header></Header>
-      <div>This is home Page</div>
+
+      <section className="wt-80 bg-black-9  flex  m-auto rounded-L">
+        <div className="  text-color-0 flex  justify-center pd-7">
+          <img
+            src="./assets/video.jpg"
+            className="object-content wt-50 "
+            alt=""
+          />
+          <h1>Here we Are with the new video release.</h1>
+        </div>
+      </section>
+      <section className="flex">
+        {" "}
+        <div class="box-shadow  flex flex-column  wt-30  rounded-s ">
+          <div>
+            <img
+              src="./assets/video.jpg"
+              class="object-content wt-100 rounded-m"
+              alt=""
+            />
+            ;
+          </div>
+
+          <div class="pd-4">
+            <div class="  ">
+              <h4>Cards vertical</h4>
+              <small>Text Secondary</small>
+            </div>
+
+            <div class="flex flex-column m-y-3 justify-btw ">
+              <button class="cursor bg-blue-5 rounded-s border-none  outline-none text-color-0 pd-x-4 pd-y-3 text-s ">
+                Primary
+              </button>
+              <button class="cursor text-color-0 bg-black-4 rounded-s outline-none border-none pd-x-4 pd-y-3 text-s m-y-                                             2">
+                Secondary
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="box-shadow  flex flex-column  wt-30  rounded-s ">
+          <div>
+            <img
+              src="./assets/video.jpg"
+              class="object-content wt-100 rounded-m"
+              alt=""
+            />
+            ;
+          </div>
+
+          <div class="pd-4">
+            <div class="  ">
+              <h4>Cards vertical</h4>
+              <small>Text Secondary</small>
+            </div>
+
+            <div class="flex flex-column m-y-3 justify-btw ">
+              <button class="cursor bg-blue-5 rounded-s border-none  outline-none text-color-0 pd-x-4 pd-y-3 text-s ">
+                Primary
+              </button>
+              <button class="cursor text-color-0 bg-black-4 rounded-s outline-none border-none pd-x-4 pd-y-3 text-s m-y-                                             2">
+                Secondary
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="box-shadow  flex flex-column  wt-30  rounded-s ">
+          <div>
+            <img
+              src="./assets/video.jpg"
+              class="object-content wt-100 rounded-m"
+              alt=""
+            />
+            ;
+          </div>
+
+          <div class="pd-4">
+            <div class="  ">
+              <h4>Cards vertical</h4>
+              <small>Text Secondary</small>
+            </div>
+
+            <div class="flex flex-column m-y-3 justify-btw ">
+              <button class="cursor bg-blue-5 rounded-s border-none  outline-none text-color-0 pd-x-4 pd-y-3 text-s ">
+                Primary
+              </button>
+              <button class="cursor text-color-0 bg-black-4 rounded-s outline-none border-none pd-x-4 pd-y-3 text-s m-y-                                             2">
+                Secondary
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="box-shadow  flex flex-column  wt-30  rounded-s ">
+          <div>
+            <img
+              src="./assets/video.jpg"
+              class="object-content wt-100 rounded-m"
+              alt=""
+            />
+            ;
+          </div>
+
+          <div class="pd-4">
+            <div class="  ">
+              <h4>Cards vertical</h4>
+              <small>Text Secondary</small>
+            </div>
+
+            <div class="flex flex-column m-y-3 justify-btw ">
+              <button class="cursor bg-blue-5 rounded-s border-none  outline-none text-color-0 pd-x-4 pd-y-3 text-s ">
+                Primary
+              </button>
+              <button class="cursor text-color-0 bg-black-4 rounded-s outline-none border-none pd-x-4 pd-y-3 text-s m-y-                                             2">
+                Secondary
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
