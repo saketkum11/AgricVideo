15d35be503a6fb0285f09049c43b909775ceb8f1
saketkum11 <kumarsaket601@gmail.com>
2022-04-25 11:49:16 +0530

    added:auth-context

--- src/Context/Auth-context/Auth-context.js
+++ src/Context/Auth-context/Auth-context.js
@@ -1,16 +1,16 @@
 import { createContext, useContext, useState } from "react";
-
+import axios from "axios";
 const authContext = createContext();
 const useAuth = () => useContext(authContext);
 
 const AuthProvider = ({ children }) => {
   const [userData, setUserData] = useState([]);
 
-  const loginHandler = async (email, password) => {
+  const loginHandler = async ({ email, password }) => {
     try {
-      const response = await axios.post(`/api/auth/signup`, {
-        email: "adarshbalika@neog.camp",
-        password: "adarshBalika",
+      const response = await axios.post(`/api/auth/login`, {
+        email: email,
+        password: password,
       });
       // saving the encodedToken in the localStorage
       localStorage.setItem("token", response.data.encodedToken);
@@ -19,7 +19,12 @@ const AuthProvider = ({ children }) => {
       console.log(error);
     }
   };
+
   return (
-    <authContext.Provider value={{ items: 9 }}>{children}</authContext.Provider>
+    <authContext.Provider value={{ loginHandler }}>
+      {children}
+    </authContext.Provider>
   );
 };
+
+export { useAuth, AuthProvider };
--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,61 +1,74 @@
 import { useState } from "react";
+import { useAuth } from "../../Context/Auth-context/Auth-context";
 
 const Login = () => {
-  const [userData, setUserData] = useState({
+  const { loginHandler } = useAuth();
+  const [values, setValues] = useState({
     email: "",
     password: "",
   });
+  const { email, password } = values;
 
   return (
     <>
       <div className=" m-auto wt-30 flex m-t-8 flex-column justify-even">
-        <div className="bg-black-1 text-color-9 justify-even flex flex-column  pd-11">
-          <label
-            htmlFor="email"
-            className="flex items-start pd-y-5 pd-x-3 text-xm text-sm text-light"
-          >
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            className=" pd-4   text-s rounded-xs "
-            placeholder="Email Id"
-            onChange={(e) => {
-              setUserData({ ...userData, email: e.target.value });
+        <div className="box-shadow-1 text-color-9 justify-even flex flex-column  pd-11">
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              loginHandler(email, password);
             }}
-          />
-
-          <label
-            htmlFor="password"
-            className="flex items-start text-xm pd-y-5 pd-x-3 text-sm text-light"
           >
-            Password{" "}
-          </label>
+            <div className="flex flex-column ">
+              {" "}
+              <label
+                htmlFor="email"
+                className=" flex items-start pd-y-5 pd-x-3 text-xm text-sm text-light"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                className=" pd-4   text-s rounded-xs "
+                placeholder="Email Id"
+                value={email}
+                onChange={(e) => {
+                  setValues({ ...values, email: e.target.value });
+                }}
+              />
+            </div>
 
-          <input
-            type="password"
-            id="password"
-            className="pd-4 text-s rounded-xs outline-none "
-            placeholder="Password"
-            onChange={(e) => {
-              setUserData({ ...userData, password: e.target.value });
-            }}
-          />
-          <div className="pd-y-9 ">
-            <button
-              type="submit"
-              className="bg-black-9 wt-100  rounded-xs border-none outline-none text-s cursor text-color-0 pd-3 text-light"
-            >
-              Login
-            </button>
-          </div>
+            <div className="flex flex-column ">
+              <label
+                htmlFor="password"
+                className="flex items-start text-xm pd-y-5 pd-x-3 text-sm text-light"
+              >
+                Password{" "}
+              </label>
 
-          <button className="bg-black-0 rounded-xs outline-none pd-2 cursor">
-            Login as Guest{" "}
-          </button>
+              <input
+                type="password"
+                id="password"
+                className="pd-4 text-s rounded-xs outline-none "
+                placeholder="Password"
+                value={password}
+                onChange={(e) => {
+                  setValues({ ...values, password: e.target.value });
+                }}
+              />
+            </div>
+
+            <div className="pd-y-9 ">
+              <button
+                type="Submit"
+                className="bg-black-9 wt-100  rounded-xs border-none outline-none text-s cursor text-color-0 pd-3 text-light"
+              >
+                Login
+              </button>
+            </div>
+          </form>
         </div>
-        <footer className="bg-black-4 "></footer>
       </div>
     </>
   );
