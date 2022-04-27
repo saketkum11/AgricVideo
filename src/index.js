7c756c69fadc10753617943e30c982c7b954ec9d
saketkum11 <kumarsaket601@gmail.com>
2022-04-27 23:16:05 +0530

    added: context

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./Context/Auth-context/Auth-context";
+import { VideoProvider } from "./Context/Video-Context/video-context";
 
 // Call make Server
 makeServer();
@@ -13,7 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <VideoProvider>
+          <App />
+        </VideoProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
