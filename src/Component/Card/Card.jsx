c7e7585e1b5a7824a026419bac12836c68888f35
saketkum11 <kumarsaket601@gmail.com>
2022-05-01 22:30:14 +0530

    removed: style

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -24,14 +24,14 @@ const Card = () => {
               <>
                 <NavLink
                   to="/"
-                  className="card flex  flex-column box-shadow rounded-m bg-black-0 text-color-9 text-dec"
+                  className="card flex  flex-column box-shadow-2 rounded-m bg-black-0 text-color-9 text-dec"
                   key={_id}
                 >
                   <div className="card_image flex ">
                     <img src={thumbnail} className=" wt-100" alt="" />
                   </div>
-                  <div className="card_content flex h-100 flex-column justify-even pd-5">
-                    <div className="card_title pd-y-2">
+                  <div className="flex h-100 flex-column justify-even pd-5">
+                    <div className="  flex flex-wrap items-start pd-y-2">
                       <span>{title}</span>
                     </div>
                     <div className="flex items-center justify-start pd-y-2">
@@ -48,12 +48,11 @@ const Card = () => {
                       </span>
                     </div>
 
-                    <div className="card_snippet"></div>
-                    <div className="card_footer">
+                    <div className=" flex flex-wrap justify-btw">
                       <button className="cursor bg-black-9  border-none  outline-none text-color-0 pd-x-4 pd-y-3 text-s ">
                         Watch now
                       </button>
-                      <div className="">
+                      <div className="flex items-center justify-btw">
                         <button className="cursor  text-color-9  border-none  outline-none text-color-0  card-icon text-s rounded-full">
                           <FaShareAlt />
                         </button>
