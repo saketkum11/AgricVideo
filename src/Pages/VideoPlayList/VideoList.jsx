48975386885150bb99e983536688afa736686ea6
saketkum11 <kumarsaket601@gmail.com>
2022-05-02 17:38:04 +0530

    added:html and style

--- src/Pages/VideoPlayList/VideoList.jsx
+++ src/Pages/VideoPlayList/VideoList.jsx
@@ -8,17 +8,26 @@ const VideoList = () => {
     <>
       <main className="flex  ">
         <SideNav />
+        <div className="flex flex-column   m-y-5 justify-center card-main">
+          <div>
+            <input
+              type="text"
+              class=" pd-4  m-y-4 text-s rounded-s search wt-50"
+              placeholder="Search"
+            />
+          </div>
 
-        <section className="flex flex-wrap  justify-center  cards">
-          {videoData &&
-            videoData.map((data) => {
-              return (
-                <>
-                  <Card key={data._id} data={data} />
-                </>
-              );
-            })}
-        </section>
+          <section className="flex flex-wrap  justify-center  cards">
+            {videoData &&
+              videoData.map((data) => {
+                return (
+                  <>
+                    <Card key={data._id} data={data} />
+                  </>
+                );
+              })}
+          </section>
+        </div>
       </main>
     </>
   );
