c9cd56ce9170901c1513161277bc8cb5f4b0e9b2
saketkum11 <kumarsaket601@gmail.com>
2022-05-03 00:32:36 +0530

    modifed: js

--- src/Context/WatchLater-context/Watch-context.js
+++ src/Context/WatchLater-context/Watch-context.js
@@ -1,27 +1,32 @@
 import axios from "axios";
 import { createContext, useContext } from "react";
-import { useEffect } from "react/cjs/react.production.min";
 import { useAuth } from "../Auth-context/Auth-context";
+
 const WatchContext = createContext();
 const useWatch = () => useContext(WatchContext);
+
 const WatchProvider = ({ children }) => {
   const { credentialData } = useAuth();
   const { tokenData, isAuth } = credentialData;
 
-  useEffect(() => {}, []);
   const getWatchLaterData = async () => {
     try {
-      const response = await axios.get("/api/user/watchlater", {
+      const response = await axios.post("/api/user/watchlater", {
         headers: {
           authorization: tokenData,
         },
       });
+      console.log("response from watch context", response);
     } catch (error) {
       console.error(error);
     }
   };
 
-  return <WatchContext.Provider value={{}}>{children}</WatchContext.Provider>;
+  return (
+    <WatchContext.Provider value={{ getWatchLaterData }}>
+      {children}
+    </WatchContext.Provider>
+  );
 };
 
 export { useWatch, WatchProvider };
