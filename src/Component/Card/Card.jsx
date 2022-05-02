9e8479b729252d72ab39bbe7066a30138007fbab
saketkum11 <kumarsaket601@gmail.com>
2022-05-03 00:32:08 +0530

    added: hmtl and style

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -1,20 +1,19 @@
-import { FaRegEye, FaShareAlt } from "react-icons/fa";
+import { FaShareAlt } from "react-icons/fa";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
-import { useVideo } from "../../Context/Video-Context/video-context";
 import "./Card.css";
+import { useWatch } from "../../Context/WatchLater-context/Watch-context";
+import { useState } from "react";
 const Card = ({ data }) => {
   const { thumbnail, title, profile, profileName } = data;
-
+  const { getWatchLaterData } = useWatch();
+  const [showToggle, setSHowToggel] = useState(false);
   return (
     <>
-      <NavLink
-        to="/"
-        className="card flex cards  flex-column box-shadow-2 rounded-m bg-black-0 text-color-9 text-dec"
-      >
-        <div className="card_image flex ">
+      <div className="card flex cards  flex-column box-shadow-2 rounded-m bg-black-0 text-color-9 text-dec position-rel">
+        <NavLink to="/" className="card_image flex ">
           <img src={thumbnail} className=" wt-100" alt="" />
-        </div>
+        </NavLink>
         <div className="flex h-100 flex-column justify-even pd-5">
           <div className="  flex flex-wrap items-start pd-y-2">
             <span>{title}</span>
@@ -37,17 +36,49 @@ const Card = ({ data }) => {
             <button className="cursor bg-black-9  border-none  outline-none text-color-0 pd-x-4 pd-y-3 text-s ">
               Watch now
             </button>
+            <div className="position-ab z-index card-drawer">
+              {showToggle ? (
+                <div class="z-index bg-black-0  m-auto justify-around flex flex-column pd-4 m-y-8  box-shadow-2">
+                  <div class="flex flex-column m-y-6">
+                    <span class="text-xm">Dialog header</span>
+                    <small>Lorem ipsum dolor sit amet.</small>
+                  </div>
+                  <div class="flex justify-end items-center m-t-2">
+                    <a
+                      href=""
+                      class="pd-y-2 pd-x-4 text-dec text-color-0  bg-black-5 rounded-m m-x-1"
+                    >
+                      close
+                    </a>
+                    <a
+                      href=""
+                      class="pd-y-2 pd-x-4 text-dec rounded-m bg-blue-5 text-color-0 m-x-1"
+                    >
+                      continue
+                    </a>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+
             <div className="flex items-center justify-btw">
               <button className="cursor  text-color-9  border-none  outline-none text-color-0  card-icon text-s rounded-full">
                 <FaShareAlt />
               </button>
-              <button className="cursor   border-none  outline-none text-color-9 text-s card-icon rounded-full ">
+              <button
+                onClick={() => {
+                  setSHowToggel(!showToggle);
+                }}
+                className="cursor   border-none  outline-none text-color-9 text-s card-icon rounded-full "
+              >
                 <BsThreeDotsVertical />
               </button>
             </div>
           </div>
         </div>
-      </NavLink>
+      </div>
     </>
   );
 };
