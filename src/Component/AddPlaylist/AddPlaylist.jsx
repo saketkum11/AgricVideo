2ec5056e66539f76300b4808aa9be0b6f2d2bce6
saketkum11 <kumarsaket601@gmail.com>
2022-05-05 18:35:52 +0530

    added: html

---
+++ src/Component/AddPlaylist/AddPlaylist.jsx
@@ -0,0 +1,35 @@
+import { usePlay } from "../../Context/Playlets-context/Playlets-context";
+
+const AddPlaylist = () => {
+  const { playlistFlag, setPlaylistFlag } = usePlay();
+  return (
+    <>
+      {playlistFlag ? (
+        <div class="bg-black-0 wt-50 m-auto justify-around flex flex-column pd-4  box-shadow-2 wt-100">
+          <div class="flex flex-column m-y-6">
+            <span class="text-xm">create playlist</span>
+          </div>
+          <div>
+            <input type="text" />
+          </div>
+          <div class="flex justify-end items-center m-t-2">
+            <button
+              onClick={() => {
+                setPlaylistFlag(!playlistFlag);
+              }}
+              class="bg-black-9 cursor rounded-s border-none  outline-none text-color-0 pd-x-3 pd-y-2"
+            >
+              close
+            </button>
+            <button class="pd-y-2 pd-x-4 outline-none text-dec rounded-m bg-blue-5 text-color-0 m-x-1">
+              continue
+            </button>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+export { AddPlaylist };
--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -6,14 +6,14 @@ import "./Card.css";
 import { useWatch } from "../../Context/WatchLater-context/Watch-context";
 import { useState } from "react";
 import { usePlay } from "../../Context/Playlets-context/Playlets-context";
+import { AddPlaylist } from "../../Pages/index";
 import { useVideo } from "../../Context/Video-Context/video-context";
 
 const Card = ({ data }) => {
   const { _id, thumbnail, title, profile, profileName } = data;
   const { addWatchLater, watchVideo, removeWatchLater } = useWatch();
-
-  const { getPlaylist, createPlaylist, setPlaylistFlag, playlistFlag } =
-    usePlay();
+  const { videoData } = useVideo();
+  const { createPlaylist, setPlaylistFlag, playlist } = usePlay();
   const [playlistTag, setPlaylistTag] = useState({
     title: "check",
   });
@@ -67,7 +67,7 @@ const Card = ({ data }) => {
                 <BsThreeDotsVertical />
               </button>
               <div className="position-ab z-index card-drawer">
-                {showToggle ? (
+                {showToggle && (
                   <div className=" bg-black-0  m-auto justify-around flex flex-column pd-4 m-y-8  box-shadow-2">
                     <div className="flex flex-column h-100">
                       <ul className="flex flex-wrap flex-column items-start ">
@@ -94,48 +94,26 @@ const Card = ({ data }) => {
                             <span className="pd-x-3">Watch Later</span>
                           </li>
                         )}
+
                         <li
                           onClick={() => {
-                            getPlaylist();
-                            setPlaylistFlag(!playlistFlag);
+                            setPlaylistFlag((previous) => !previous);
                           }}
                           className="style-none flex items-centers pd-y-2 cursor"
                         >
                           <MdPlaylistAdd className="text-xm"></MdPlaylistAdd>
                           <span className="pd-x-3">Add to playlist</span>
                         </li>
-                        <div className="z-index position-ab card-modal  flex">
-                          {playlistFlag ? (
-                            <div class="bg-black-0 wt-50 m-auto justify-around flex flex-column pd-4  box-shadow-2 wt-100">
-                              <div class="flex flex-column m-y-6">
-                                <span class="text-xm">create playlist</span>
-                              </div>
-                              <div>
-                                <input type="text" />
-                              </div>
-                              <div class="flex justify-end items-center m-t-2">
-                                <button
-                                  onClick={() => {
-                                    setPlaylistFlag(!playlistFlag);
-                                  }}
-                                  class="bg-black-9 cursor rounded-s border-none  outline-none text-color-0 pd-x-3 pd-y-2"
-                                >
-                                  close
-                                </button>
-                                <button class="pd-y-2 pd-x-4 outline-none text-dec rounded-m bg-blue-5 text-color-0 m-x-1">
-                                  continue
-                                </button>
-                              </div>
-                            </div>
-                          ) : (
-                            ""
-                          )}
-                        </div>
+                        {videoData.some((video) => video._id === _id) ? (
+                          <div className="z-index position-ab card-modal  flex">
+                            <AddPlaylist></AddPlaylist>
+                          </div>
+                        ) : (
+                          ""
+                        )}
                       </ul>
                     </div>
                   </div>
-                ) : (
-                  ""
                 )}
               </div>
             </div>
